a59de590ca8bc421517419614b60ebdb0286037b
Ferdomi21 <fernandodominguelago@gmail.com>
2023-05-08 10:42:08 +0200

    Atributos y Constructor

--- prBanco/src/prBanco/Banco.java
+++ prBanco/src/prBanco/Banco.java
@@ -2,4 +2,13 @@ package prBanco;
 
 public class Banco {
 
+	private int iban;
+	private double saldo;
+	
+	public Banco(int iban, double saldo) {
+		this.iban = iban;
+		this.saldo = saldo;
+	}
+	
+	
 }
