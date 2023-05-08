5365342e95c751e533a864f4627b44d4f7a98185
AlbertoF-DAW <aff0017@alu.medac.es>
2023-05-08 11:16:27 +0200

    Modificada clase Operaciones

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -2,20 +2,20 @@ package prBanco;
 
 public abstract class Operaciones {
 
-	public void prestamo(Cuenta cuenta, double cantidad) {
+	public static void prestamo(Cuenta cuenta, double cantidad) {
 		cuenta.setSaldo(cuenta.getSaldo() + cantidad);
 	}
 	
-	public void transferencia(Cuenta c1, Cuenta c2, double cantidad) {
+	public static void transferencia(Cuenta c1, Cuenta c2, double cantidad) {
 		c1.setSaldo(c1.getSaldo() - cantidad);
 		c2.setSaldo(c2.getSaldo() + cantidad);
 	}
 	
-	public void bloquear(Cuenta cuenta) {
+	public static void bloquear(Cuenta cuenta) {
 		cuenta.setEstado("Bloqueada");
 	}
 	
-	public void desbloquear(Cuenta cuenta) {
+	public static void desbloquear(Cuenta cuenta) {
 		cuenta.setEstado("Activa");
 	}
 }
