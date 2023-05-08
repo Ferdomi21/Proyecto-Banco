3432b405c9a4a12ae208ff5ee1675e110cd3253c
AlbertoF-DAW <aff0017@alu.medac.es>
2023-05-08 11:02:58 +0200

    Metodos añadidos a Operaciones

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -1,5 +1,21 @@
 package prBanco;
 
-public class Operaciones {
+public abstract class Operaciones {
 
+	public void prestamo(Cuenta cuenta, double cantidad) {
+		cuenta.setSaldo(cuenta.getSaldo() + cantidad);
+	}
+	
+	public void transferencia(Cuenta c1, Cuenta c2, double cantidad) {
+		c1.setSaldo(c1.getSaldo() - cantidad);
+		c2.setSaldo(c2.getSaldo() + cantidad);
+	}
+	
+	public void bloquear(Cuenta cuenta) {
+		cuenta.setEstado("Bloqueada");
+	}
+	
+	public void desbloquear(Cuenta cuenta) {
+		cuenta.setEstado("Activa");
+	}
 }
