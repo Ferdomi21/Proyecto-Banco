921b32d8d6a12497926e46ced5438f81efd0d8e0
AlbertoF-DAW <aff0017@alu.medac.es>
2023-05-08 10:44:39 +0200

    Constructor cliente

--- prBanco/src/prBanco/Cliente.java
+++ prBanco/src/prBanco/Cliente.java
@@ -1,5 +1,13 @@
 package prBanco;
 
 public class Cliente {
-
+	private String nombre;
+	private String apellidos;
+	
+	public Cliente(String nombre, String apellidos) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+	
+	
 }
