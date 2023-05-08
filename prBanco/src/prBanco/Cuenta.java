967a49cbc217a1d88dd3f341c1b719e43094d5a5
AlbertoF-DAW <aff0017@alu.medac.es>
2023-05-08 10:50:24 +0200

    Arreglados errores clases Cuenta y Cliente

--- prBanco/src/prBanco/Cuenta.java
+++ prBanco/src/prBanco/Cuenta.java
@@ -4,10 +4,12 @@ public class Cuenta {
 
 	private int iban;
 	private double saldo;
+	private String estado;
 	
-	public Cuenta(int iban, double saldo) {
+	public Cuenta(int iban, double saldo, String estado) {
 		this.iban = iban;
 		this.saldo = saldo;
+		this.estado = estado;
 	}
 	
 	
